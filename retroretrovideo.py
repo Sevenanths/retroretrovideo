6253caeceee682c5fb52db7d4f374d39d3cf54b3
Sevenanths <anthernate@gmail.com>
2024-11-09 19:37:58 +0100

    Allow outside connections

--- retroretrovideo.py
+++ retroretrovideo.py
@@ -54,4 +54,4 @@ def latest_screenshot():
     return send_file(SCREENSHOT_PATH, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=args.port)
+    app.run(debug=True, port=args.port, host="0.0.0.0")
